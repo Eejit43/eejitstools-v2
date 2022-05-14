dd9d4d1258b87d726b40807feb5cc61647601688
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-05-14 13:40:48 -0400

    reworded vpn text

--- public/scripts/pages/ip-info.js
+++ public/scripts/pages/ip-info.js
@@ -5,7 +5,7 @@ import { twemojiUpdate } from '/scripts/functions.js';
 fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=7dcea861f91a44579cea73ad833eff21').then(async (response) => {
     const data = await response.json();
 
-    document.getElementById('ip-address').innerHTML = `${data.ip_address} (${data.security.is_vpn ? 'Is' : 'Not'} a <span class="tooltip-bottom" data-tooltip="Virtual private network">VPN</span><sup class="tooltip-top" style="text-decoration: none" data-tooltip="May not be accurate">*</sup>)`;
+    document.getElementById('ip-address').innerHTML = `${data.ip_address} (${data.security.is_vpn ? 'Is' : 'Not'} a known <span class="tooltip-bottom" data-tooltip="Virtual private network">VPN</span>)`;
     document.getElementById('isp').innerHTML = `${data.connection.isp_name} ${data.connection.isp_name !== data.connection.organization_name ? `(${data.connection.organization_name}) ` : ''}(${data.connection.connection_type})`;
     document.getElementById('location').innerHTML = `${data.city} (${data.postal_code}), ${data.region ? `${data.region}, ` : ''}${data.country}`;
     document.getElementById('latitude').innerHTML = data.latitude;
