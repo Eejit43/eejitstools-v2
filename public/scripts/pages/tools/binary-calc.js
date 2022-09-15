3f3d211e6a91f585e128213a503cfdf8b4068ca5
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-09-14 22:38:29 -0400

    support decimals in `binary-calc`

--- public/scripts/pages/tools/binary-calc.js
+++ public/scripts/pages/tools/binary-calc.js
@@ -51,19 +51,19 @@ function findInput() {
     if (input.value.length > 0) {
         switch (radices[inputType.value].number) {
             case 2:
-                if (/^-?[01]+$/.test(input.value)) convert(input.value, 2);
+                if (/^-?[01.]+$/.test(input.value)) convert(input.value, 2);
                 else notValid();
                 break;
             case 8:
-                if (/^-?[0-7]+$/.test(input.value)) convert(input.value, 8);
+                if (/^-?[0-7.]+$/.test(input.value)) convert(input.value, 8);
                 else notValid();
                 break;
             case 10:
-                if (/^-?[0-9]+$/.test(input.value)) convert(input.value, 10);
+                if (/^-?[0-9.]+$/.test(input.value)) convert(input.value, 10);
                 else notValid();
                 break;
             case 16:
-                if (/^-?[0-9a-fA-F]+$/.test(input.value)) convert(input.value, 16);
+                if (/^-?[0-9a-fA-F.]+$/.test(input.value)) convert(input.value, 16);
                 else notValid();
                 break;
             default:
@@ -80,11 +80,16 @@ function findInput() {
 
 /**
  * Converts the given value and displays the converted value
- * @param {number} value the value to convert
+ * @param {string} value the value to convert
  * @param {number} radix the base of the input value
+ * @returns {void}
  */
 function convert(value, radix) {
-    let result = parseInt(value, radix).toString(radices[outputType.value].number);
+    if (value.match(/\./g)?.length > 1) return notValid();
+
+    let result = parseNumberWithRadix(value, radix).toString(radices[outputType.value].number);
+
+    if (!result || isNaN(result)) return notValid();
 
     switch (radices[outputType.value].name) {
         case 'binary':
@@ -104,6 +109,19 @@ function convert(value, radix) {
 }
 
 /**
+ * Converts a string to an integer/float
+ * @param {string} number the number to convert
+ * @param {number} [radix=10] the base of the number
+ * @returns {number} the converted number
+ */
+function parseNumberWithRadix(number, radix) {
+    radix = (radix || 10) | 0;
+    const [a, b] = number.split('.');
+    const l = parseInt('1' + (b || ''), radix).toString(radix).length;
+    return parseInt(a, radix) + parseInt(b || '0', radix) / parseInt('1' + Array(l).join('0'), radix);
+}
+
+/**
  * Handles an invalid input
  */
 function notValid() {
@@ -119,16 +137,22 @@ function notValid() {
  * @returns {string} the binary number with spaces
  */
 function addBinarySpacers(binary) {
-    console.log(binary);
     const sign = binary.startsWith('-') ? '-' : '';
-    return (
-        sign +
-        binary
-            .replace(/^-/, '')
-            .padStart(Math.ceil(binary.length / 4) * 4, '0')
-            .match(/\d{4}/g)
-            .join(' ')
-    );
+
+    binary = binary.replace(/^-/, '').split('.');
+
+    let result = binary[0]
+        .padStart(Math.ceil(binary[0].length / 4) * 4, '0')
+        .match(/[01]{4}/g)
+        .join(' ');
+
+    if (binary.length > 1)
+        result += `.${binary[1]
+            .padEnd(Math.ceil(binary[1].length / 4) * 4, '0')
+            .match(/[01]{4}/g)
+            .join(' ')}`;
+
+    return sign + result;
 }
 
 const radices = {
