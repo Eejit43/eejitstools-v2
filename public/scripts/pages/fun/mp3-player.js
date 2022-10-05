654f6eed6e6cbc0c0651104cec9670bc24bb3d25
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-10-05 18:18:19 -0400

    rework shuffle functionality for `mp3-player`

--- public/scripts/pages/fun/mp3-player.js
+++ public/scripts/pages/fun/mp3-player.js
@@ -82,16 +82,20 @@ function createTrackItem(category, index, track) {
 }
 
 audioTracks.forEach((category) => {
-    const sectionTitleLink = document.createElement('a');
-    sectionTitleLink.href = `#${category.id}`;
-
     const sectionTitle = document.createElement('div');
     sectionTitle.classList.add('playlist-section-title');
     sectionTitle.id = category.id;
-    sectionTitle.textContent = category.name;
 
-    sectionTitleLink.appendChild(sectionTitle);
-    playlist.appendChild(sectionTitleLink);
+    const sectionTitleLink = document.createElement('a');
+    sectionTitleLink.href = `#${category.id}`;
+    sectionTitleLink.textContent = category.name;
+
+    const playSectionButton = document.createElement('i');
+    playSectionButton.classList.add('player-icon', 'fa-solid', 'fa-shuffle');
+
+    sectionTitle.appendChild(sectionTitleLink);
+    sectionTitle.appendChild(playSectionButton);
+    playlist.appendChild(sectionTitle);
 
     const playlistsListItem = document.createElement('li');
 
@@ -104,6 +108,10 @@ audioTracks.forEach((category) => {
     playlistsList.appendChild(playlistsListItem);
 
     category.tracks.forEach((track, index) => createTrackItem(category, index, track));
+});
+
+document.querySelectorAll('.playlist-section-title > i.player-icon').forEach((playButton) => {
+    playButton.addEventListener('click', () => shuffleSection(playButton.parentElement.id));
 });
 
 let audioCategory = 'general';
@@ -126,15 +134,18 @@ function loadNewTrack(category, index, play = true) {
     audioIndex = index;
 }
 
-const loadedTracks = document.querySelectorAll('.playlist-track');
-
-for (const track of loadedTracks) track.addEventListener('click', loadClickedTrack);
+for (const track of document.querySelectorAll('.playlist-track')) track.addEventListener('click', loadClickedTrack);
 
 /**
  * Loads the clicked track
  * @param {MouseEvent} event the click event
  */
 function loadClickedTrack(event) {
+    shuffled = false;
+    shuffledQueue = [];
+    shuffleStatusIcon.classList.remove('fa-check');
+    shuffleStatusIcon.classList.add('fa-xmark');
+
     const { category, index } = event.target.dataset;
     if (category === audioCategory && index === audioIndex) toggleAudio();
     else loadNewTrack(category, index);
@@ -150,19 +161,64 @@ function toggleAudio() {
         playPauseButton.classList.remove('fa-play');
         playPauseButton.classList.add('fa-pause');
         document.getElementById(`playlist-track-${audioCategory}-${audioIndex}`).classList.add('active-track');
-        playToPause(audioCategory, audioIndex);
+        pauseToPlay(audioCategory, audioIndex);
         audio.play();
     } else {
         playPauseButton.classList.add('fa-play');
         playPauseButton.classList.remove('fa-pause');
-        pauseToPlay(audioCategory, audioIndex);
+        playToPause(audioCategory, audioIndex);
         audio.pause();
     }
 }
 
+let shuffledQueue = [];
+
+/**
+ * Shuffles an audio section
+ * @param {string} category the section category to shuffle
+ */
+function shuffleSection(category) {
+    const oldCategory = audioCategory,
+        oldIndex = audioIndex;
+
+    shuffled = true;
+    shuffleStatusIcon.classList.remove('fa-xmark');
+    shuffleStatusIcon.classList.add('fa-check');
+    if (!audio.paused) {
+        playPauseButton.classList.add('fa-play');
+        playPauseButton.classList.remove('fa-pause');
+        playToPause(audioCategory, audioIndex);
+        audio.pause();
+    }
+    audioCategory = category;
+
+    const tracks = tracksByCategory[category].tracks.map((track, index) => ({ ...track, index }));
+    shuffledQueue = shuffleArray(tracks);
+
+    audioIndex = tracks[0].index;
+
+    updateActiveTrackStyle(oldCategory, oldIndex, audioCategory, audioIndex, false);
+
+    loadNewTrack(audioCategory, audioIndex, true);
+
+    shuffledQueue.shift();
+}
+
+/**
+ * Shuffles the order of items in an array
+ * @param {Array} array The array to shuffle
+ * @returns {Array} shuffled array
+ */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 /**
  * Updates the progress bar
- * @returns {void}
  */
 function onTimeUpdate() {
     timer.textContent = formatTime(audio.currentTime);
@@ -170,10 +226,12 @@ function onTimeUpdate() {
     if (audio.ended) {
         playPauseButton.classList.add('fa-play');
         playPauseButton.classList.remove('fa-pause');
-        pauseToPlay(audioCategory, audioIndex);
+        playToPause(audioCategory, audioIndex);
 
-        if (shuffled) return loadNewTrack(audioCategory, Math.floor(Math.random() * tracksByCategory[audioCategory].tracks.length));
-        if (audioIndex < tracksByCategory[audioCategory].tracks.length - 1) loadNewTrack(audioCategory, parseInt(audioIndex) + 1);
+        if (shuffled && shuffledQueue.length > 0) {
+            const nextTrack = shuffledQueue.shift();
+            loadNewTrack(audioCategory, nextTrack.index);
+        } else if (audioIndex < tracksByCategory[audioCategory].tracks.length - 1) loadNewTrack(audioCategory, parseInt(audioIndex) + 1);
     }
 }
 
@@ -258,13 +316,13 @@ function previous() {
  * @param {number} oldIndex the index of the old track
  * @param {string} newCategory the category of the new track
  * @param {number} newIndex the index of the new track
- * @param {boolean} [play=true] whether or not to mark the track as playing
+ * @param {boolean} [play=true] whether or not to mark the new track as playing
  */
 function updateActiveTrackStyle(oldCategory, oldIndex, newCategory, newIndex, play = true) {
     document.getElementById(`playlist-track-${oldCategory}-${oldIndex}`).classList.remove('active-track');
-    if (play) pauseToPlay(oldCategory, oldIndex);
+    if (play) playToPause(oldCategory, oldIndex);
     document.getElementById(`playlist-track-${newCategory}-${newIndex}`).classList.add('active-track');
-    if (play) playToPause(newCategory, newIndex);
+    if (play) pauseToPlay(newCategory, newIndex);
 }
 
 let shuffled = false;
@@ -285,22 +343,22 @@ function toggleShuffle() {
 }
 
 /**
- * Pauses the playing audio
+ * Marks the icon of a track as playing
  * @param {string} category the category of the track
  * @param {number} index the index of the track
  */
-function playToPause(category, index) {
+function pauseToPlay(category, index) {
     const element = document.getElementById(`player-icon-${category}-${index}`);
     element.classList.remove('fa-play');
     element.classList.add('fa-pause');
 }
 
 /**
- * Plays the paused audio
+ * Marks the icon of a track as paused
  * @param {string} category the category of the track
  * @param {number} index the index of the track
  */
-function pauseToPlay(category, index) {
+function playToPause(category, index) {
     const element = document.getElementById(`player-icon-${category}-${index}`);
     element.classList.add('fa-play');
     element.classList.remove('fa-pause');
@@ -322,7 +380,7 @@ function toggleMute() {
 }
 
 document.addEventListener('keydown', (event) => {
-    if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') return;
+    if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA' || event.ctrlKey || event.metaKey) return;
 
     if (event.code === 'Space') {
         event.preventDefault();
