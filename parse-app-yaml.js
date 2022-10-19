1500f29ee42631d8fde5e79f57d17fbb4c9d6cf8
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-10-19 17:35:34 -0400

    improve `parse-app-yaml` script

--- parse-app-yaml.js
+++ parse-app-yaml.js
@@ -3,15 +3,14 @@ import yaml from 'js-yaml';
 
 const data = yaml.load(fs.readFileSync('./app-temp.yaml', 'utf8'));
 const env = fs.readFileSync('./.env', 'utf8');
-const envParsed = env
-    .split('\n')
-    .filter((line) => line)
-    .reduce((acc, line) => {
-        const [key, value] = line.split('=');
-        acc[key.trim()] = value.trim();
-        return acc;
-    }, {});
+
+const envParsed = Object.fromEntries(
+    env
+        .split('\n')
+        .filter((line) => line && line.includes('='))
+        .map((line) => [line.split('=')[0].trim(), line.split('=')[1].trim()])
+);
 
 data.env_variables = envParsed; // eslint-disable-line camelcase
 
-fs.writeFileSync('./app.yaml', yaml.dump(data));
+fs.writeFileSync('./app.yaml', yaml.dump(data, { indent: 4 }));
