fe045e190a65253303fea4ddefbdc6a6f16f381e
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-10-27 15:17:19 -0400

    attempt to resolve railway hosting issue

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ fastify.setNotFoundHandler((request, reply) => {
 const port = process.env.PORT || 3000;
 
 // Start server
-fastify.listen({ port }, (error) => {
+fastify.listen({ port, host: '0.0.0.0' }, (error) => {
     if (error) {
         fastify.log.error(error);
         process.exit(1);
