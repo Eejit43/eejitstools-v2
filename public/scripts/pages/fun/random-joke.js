438c6a4ae2381bea3c9e445d1bd35d36bcbde77f
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-05-16 09:00:07 -0400

    `fun/random-joke`: fix line breaks

--- public/scripts/pages/fun/random-joke.js
+++ public/scripts/pages/fun/random-joke.js
@@ -13,7 +13,7 @@ async function fetchJoke() {
     const response = await fetch('https://v2.jokeapi.dev/joke/Miscellaneous,Pun?safe-mode');
     const data = await response.json();
 
-    joke.textContent = data.joke ? data.joke.replace(/\n/g, '<br />') : `${data.setup.replace(/\n/g, '<br />')}<br />${data.delivery.replace(/\n/g, '<br />')}`;
+    joke.innerHTML = data.joke ? data.joke.replace(/\n/g, '<br />') : `${data.setup.replace(/\n/g, '<br />')}<br />${data.delivery.replace(/\n/g, '<br />')}`;
 }
 
 fetchJoke();
